3bb606c2b51343e313e474137692c473f5457e5c
jmccormick41 <jmccormick41@centurylink.net>
2016-04-24 07:36:45 -0600

    first commit

--- js/app.js
+++ js/app.js
@@ -1,49 +1,55 @@
-$ (document).ready (function(){
 
+$(document).ready (function() {
 
 
 //question objects
 var questions = [
 {
   qNum: 1,
-  q: "Who is the all time Home Run Leader in Major League baseballJ",
-  choices: ["Hank Aaron", "Barry Bonds", "Babe Ruth", "Mark McGuire"],
+  q: "Who won the World Series in Major League Baseball in 2015?",
+  choices: ["New York Mets", "Kansas City Royals", "Los Angeles Dodgers", "New York Yankees"],
   correct: 2,
-  definition: "Barry Bonds"
 },
 
 {
   qNum: 2,
-  q: "What is a practitioner of labiomancy good at?", 
-  choices: ["Finding water with a divining rod.", "Chemistry.", "Storm chasing.", "Reading lips"],
+  q: "Which U.S. President served more than 2 terms?", 
+  choices: ["Ronald Reagan", "George W Bush", "Abraham Lincoln", "Franklin Delano Roosevelt"],
   correct: 4,
-  definition: "n. Divination or interpretation by means of the motions of the lips; specifically, lip-reading."
+  
 },
 
  {
   qNum: 3,
-  q: "When would you eat a mesonoxian feast?",
-  choices: ["At sunset on the Vernal equinox.", "750 BC.", "At midnight.", " On your  fiesta de quinceañera"],
+  q: "Who won his first Oscar in 2015?",
+  choices: ["Dustin Hoffman", "Robin Williams", "Leonardo Dicaprio", "Julia Roberts"],
   correct: 3,
-  definition: "adj. Pertaining to the hour of midnight."
+
 },
 
 {
   qNum: 4,
-  q: "Your grandmother gave you spondulicks…what is it?",
-  choices: ["Cash.", "A contagious disease.", "A delicious treat.", "Her homemade remedy for what ails you."],
+  q: "Who is the richest man in the United States?",
+  choices: ["Bill Gates", "Warren Buffet", "Mark Zuckerburg", "Larry Ellison"],
   correct: 1,
-  definition: "n. Money."
+  
 },
 
 {
   qNum: 5,
-  q: "Where would you wear your winklepickers?",
-  choices: ["On the bridge of your nose.", "On your feet.", "On your hands.", "That's personal!."],
+  q: "What is the Capital of New Mexico??",
+  choices: ["Albuquerque", "Santa Fe", "Carlsbad", "Roswell"],
   correct: 2,
-  definition: "n. A shoe with a long pointed toe, popular in the 1950s."
+  
 },
 
+{
+  qNum: 6,
+  q: "YWho won the Super Bowl in 2016",
+  choices: ["Carolina Panthers", "Green Bay Packers", "Arizona Cardinals", "Denver Broncos"],
+  correct: 4,
+  
+}
 ];
 
 //global variables
@@ -56,10 +62,10 @@ var wrongText = "That's incorrect";
 
 
 // on clicking begin
-  $(".begin").click(function() {
+  $("#begin").click(function() {
     //  hide instructions
-    $(".instructions").fadeOut("fast");
-    $(".quiz").show("slow", getQuestion);
+    $("#instructions").fadeOut("fast");
+    $("#quiz").show("slow", getQuestion);
   });
   
 // starting new game function
@@ -67,10 +73,10 @@ var wrongText = "That's incorrect";
     currentQuestion = 1;
     correctAnswers = 0;
     questionIndex = 0;
-    $(".question").show();
-    $(".choices").show();
-    $(".submitAnswer").show();
-    $(".playAgain").hide();
+    $("#question").show();
+    $("#choices").show();
+    $("#submitAnswer").show();
+    $("#playAgain").hide();
     $('input:radio[name=radios]').attr('checked',false);
     getQuestion();
  };
@@ -78,12 +84,12 @@ var wrongText = "That's incorrect";
  //get question and choices, show question #
  function getQuestion() {
   // questionIndex++;
-  $(".currentQ").text(questions[questionIndex].q);
-  $(".choice0").text(questions[questionIndex].choices[0]);
-  $(".choice1").text(questions[questionIndex].choices[1]);
-  $(".choice2").text(questions[questionIndex].choices[2]);
-  $(".choice3").text(questions[questionIndex].choices[3]);
-  $(".count").text("Question " + currentQuestion + " of 6");
+  $("#currentQ").text(questions[questionIndex].q);
+  $("#choice0").text(questions[questionIndex].choices[0]);
+  $("#choice1").text(questions[questionIndex].choices[1]);
+  $("#choice2").text(questions[questionIndex].choices[2]);
+  $("#choice3").text(questions[questionIndex].choices[3]);
+  $("#count").text("Question " + currentQuestion + " of 6");
  };
 
 
@@ -106,18 +112,18 @@ var wrongText = "That's incorrect";
     // if right answer
     else if (radioValue == questions[questionIndex].correct) {
       // show "correct"
-      $(".submitAnswer").html('<h2>' + correctText + '</h2>');
+      $("#submitAnswer").html('<h2>' + correctText + '</h2>');
       // show definition
-      $(".definition").show()
-      $(".definition").text(questions[questionIndex].definition);
+      $("#definition").show()
+      $("#definition").text(questions[questionIndex].definition);
       correctAnswers++;
 
     } else {
       // if wrong answer show "wrong"
-      $(".submitAnswer").html('<h2>' + wrongText + '</h2>');
+      $("#submitAnswer").html('<h2>' + wrongText + '</h2>');
       // show definition
-      $(".definition").show()
-      $(".definition").text(questions[questionIndex].definition);
+      $("#definition").show()
+      $("#definition").text(questions[questionIndex].definition);
     
     };
     //  show next button
@@ -125,40 +131,40 @@ var wrongText = "That's incorrect";
 
       if (radioValue == questions[questionIndex].correct) {
         // show "correct"
-        $(".submitAnswer").html('<h2>' + correctText + '</h2>');
+        $("#submitAnswer").html('<h2>' + correctText + '</h2>');
         // show definition
-        $(".definition").show()
-        $(".definition").text(questions[questionIndex].definition);
+        $("#definition").show()
+        $("#definition").text(questions[questionIndex].definition);
 
         } else {
       // if wrong answer show "wrong"
-        $(".submitAnswer").html('<h2>' + wrongText + '</h2>');
+        $("#submitAnswer").html('<h2>' + wrongText + '</h2>');
       
         }
 
-        $(".nextQ").hide(); 
-        $(".count").text("Congrats! You got " + correctAnswers + " out of 6 correct!");
+        $("#nextQ").hide(); 
+        $("#count").text("Congrats! You got " + correctAnswers + " out of 6 correct!");
         // "play again" option
-        $(".playAgain").show();
+        $("#playAgain").show();
       
         // $("#choices").hide();
-        $(".submitAnswer").hide();
+        $("#submitAnswer").hide();
 
     } else {
-        $(".nextQ").show();
+        $("#nextQ").show();
     };
 
     
 
   }; //end checkAnswer function
 
-  $(".nextQ").click(function() {
+  $("#nextQ").click(function() {
       nextQuestion();
       $('input:radio[name=radios]').attr('checked',false);
     });
 
   //  click check answer
-  $(".submitAnswer").click(function() {
+  $("#submitAnswer").click(function() {
       checkAnswer();
   });
 
@@ -167,10 +173,10 @@ var wrongText = "That's incorrect";
 function nextQuestion() {
 
   // hide next button
-  $(".nextQ").hide();
+  $("#nextQ").hide();
   //show check answer option
-  $(".submitAnswer").html('<h2>Check Answer</h2>');
-  $(".definition").hide();
+  $("#submitAnswer").html('<h2>Check Answer</h2>');
+  $("#definition").hide();
   currentQuestion++;
   questionIndex++;
   getQuestion();
@@ -179,12 +185,11 @@ function nextQuestion() {
 
     //play again function
 
-    $(".playAgain").click(function() {
-      $(".submitAnswer").html('<h2>Check Answer</h2>');
-      $(".nextQ").show();
-      $(".definition").hide();
+    $("#playAgain").click(function() {
+      $("#submitAnswer").html('<h2>Check Answer</h2>');
+      $("#nextQ").show();
+      $("#definition").hide();
       startGame();
     });
 
 }); // end document ready function
-
